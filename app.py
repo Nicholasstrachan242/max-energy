9eaa1a0564b3efad4d64059256fd592d3f3cabe0
= <=>
2025-06-05 15:04:44 -0400

    move html styling to css

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from routes import home_bp as home, login_bp as login
 app = Flask(__name__)
 
 app.register_blueprint(home)
-app.register_blueprint(login)   
+app.register_blueprint(login)
 
 if __name__ == '__main__':
     app.run(debug=True)
