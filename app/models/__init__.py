70e9e96afa1561a957db32aefad4e87d35a42cc1
= <=>
2025-06-16 18:01:53 -0400

    add comments about models and flask-migrate for future reference

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,3 +3,15 @@
 
 from .User import User 
 from .Names import Names
+
+# To migrate any new models or changes to existing schema, run the following Flask-Migrate commands:
+# flask db migrate -m "message here"     -- this creates a new migration script
+# flask db upgrade                       -- this runs the migration script and updates the database
+
+# additional commands:
+# flask db history                       -- this shows the history of migrations
+# flask db current                       -- this shows the current version of the database
+# flask db downgrade                     -- this reverts the database to the previous version
+# flask db stamp head                    -- this marks the current version as the head
+# flask db history                       -- this shows the history of migrations
+
